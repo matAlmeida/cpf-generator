8cf28854690f88b6ac5bd4ca5a3311fabe11778e
Matheus Almeida <matheus.almeida@natahouse.com>
2021-05-26 01:09:37 -0300

    fix: correct version variable

--- cpf_generator/CPF.py
+++ cpf_generator/CPF.py
@@ -93,7 +93,7 @@ def generate():
     return baseCpf + str(check1) + str(check2)
 
 @deprecation.deprecated(deprecated_in="2.1.0", removed_in="3.0.0",
-                        current_version=version(__name__),
+                        current_version=__version__,
                         details="Use the 'format' function instead")
 def formater(cpf):
     format(cpf)
