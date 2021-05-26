0ca9f6e99e5e5413f1fe8d0b3d2002faf06b3610
Matheus Almeida <matheus.almeida@natahouse.com>
2021-05-26 00:34:36 -0300

    v2.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cpf_generator",
-    version="2.0.2",
+    version="2.0.3",
     author="Matheus Almeida",
     author_email="mat.almeida@live.com",
     description="Generate, Validate and Format brazilian CPF",
